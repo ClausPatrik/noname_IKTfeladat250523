812de200bb84ae0a56229b4b6a7d1b0f30663237
ClausPatrik <93657533+ClausPatrik@users.noreply.github.com>
2025-05-23 09:45:26 +0200

    250523
    -grafikus felület
    -class

---
+++ noname/autokmozgasa_250523/Adat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autokmozgasa_250523
+{
+    internal class Adat
+    {
+
+            public string Rendszam { get; set; }
+            public int Honap { get; set; }
+            public int Nap { get; set; }
+            public int Ido { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Rendszam} - {Honap}.{Nap} - {Ido} mp";
+            }
+        }
+
+    }
+
---
+++ noname/autokmozgasa_250523/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace autokmozgasa_250523
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Adat> adatok = new List<Adat>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BetoltAdatok();
+            ListaBox.ItemsSource = adatok;
+
+        }
+
+        private void BetoltAdatok()
+        {
+            var sorok = File.ReadAllLines("jeladas.txt");
+            foreach (var sor in sorok)
+            {
+                var mezok = sor.Split('\t');
+                if (mezok.Length == 4)
+                {
+                    adatok.Add(new Adat
+                    {
+                        Rendszam = mezok[0],
+                        Honap = int.Parse(mezok[1]),
+                        Nap = int.Parse(mezok[2]),
+                        Ido = int.Parse(mezok[3])
+                    });
+                }
+            }
+        }
+
+        private void Kereses_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
